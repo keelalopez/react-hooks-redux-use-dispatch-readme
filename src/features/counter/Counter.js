dc3424513230afdb0827e2ffda05a9351dbef10e
Keila Lopez <keilalofra@gmail.com>
2023-02-22 18:31:05 -0800

    completed lab.

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { incrementCount } from "./counterSlice.js";
 
 function Counter() {
   // read from the Redux store
@@ -9,8 +10,14 @@ function Counter() {
   const dispatch = useDispatch();
 
   function handleOnClick() {
-    // dispatching an action on click
+    // MANUALLY ADDED. NOT RECOMMENDED. 
     dispatch({ type: "count/increment" });
+  }
+
+  // USING SEPARATION OF CONCERNS, IMPORT FUNCTION
+  function handleOnClick() {
+    // dispatching an action on click
+    dispatch(incrementCount());
   }
 
   return (
